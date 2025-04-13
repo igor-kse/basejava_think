615a53eb0149a5b92fa9db98fd5ae600d0ba0742
Igor K <kopyshev.se@gmail.com>
2025-04-13 04:33:56 +0400

    [Enhancement] File storage directory cleanup

--- src/test/java/ru/javaops/storage/file/AbstractFileStorageTest.java
+++ src/test/java/ru/javaops/storage/file/AbstractFileStorageTest.java
@@ -1,14 +1,27 @@
 package ru.javaops.storage.file;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import ru.javaops.model.Resume;
 import ru.javaops.storage.AbstractStorageTest;
 import ru.javaops.storage.Storage;
 
+import java.io.File;
+
 public abstract class AbstractFileStorageTest extends AbstractStorageTest {
 
-    public AbstractFileStorageTest(Storage storage) {
+    private static File STORAGE_DIRECTORY;
+
+    public AbstractFileStorageTest(Storage storage, String storageDir) {
         super(storage);
+        STORAGE_DIRECTORY = new File(storageDir);
+    }
+
+    @AfterAll
+    protected static void cleanUp() {
+        if (STORAGE_DIRECTORY != null && STORAGE_DIRECTORY.exists() && !STORAGE_DIRECTORY.delete()) {
+            throw new RuntimeException("Could not delete " + STORAGE_DIRECTORY.getAbsolutePath());
+        }
     }
 
     @Override
--- src/test/java/ru/javaops/storage/file/ObjectStreamStorageTest.java
+++ src/test/java/ru/javaops/storage/file/ObjectStreamStorageTest.java
@@ -2,7 +2,9 @@ package ru.javaops.storage.file;
 
 public class ObjectStreamStorageTest extends AbstractFileStorageTest {
 
+    private static final String STORAGE_DIRECTORY = ".\\objectStreamStorage";
+
     public ObjectStreamStorageTest() {
-        super(new ObjectStreamStorage(".\\objectStreamStorage"));
+        super(new ObjectStreamStorage(STORAGE_DIRECTORY), STORAGE_DIRECTORY);
     }
 }
